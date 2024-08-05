1946e91d8f7d6a358b7d3d928bd99b7adeb3e2dd
ALIMUHAMMADALIHABAT <habatali64@gmail.com>
2024-08-05 22:01:28 +0300

    add imge onbarding

--- lib/core/constant/image.dart
+++ lib/core/constant/image.dart
@@ -3,6 +3,18 @@ class Assets {
   
   /// Assets for assetsImgLogoipsum255 1
   /// assets/img/logoipsum-255 1.svg
-  static const String assetsImgLogoipsum2551 = "assets/img/logoipsum-255 1.svg";
+  static const String assetsImgLogoipsum255 1 = "assets/img/logoipsum-255 1.svg";
+
+  /// Assets for assetsImgOnb1
+  /// assets/img/onb1.svg
+  static const String assetsImgOnb1 = "assets/img/onb1.svg";
+
+  /// Assets for assetsImgOnb2
+  /// assets/img/onb2.svg
+  static const String assetsImgOnb2 = "assets/img/onb2.svg";
+
+  /// Assets for assetsImgOnb3
+  /// assets/img/onb3.svg
+  static const String assetsImgOnb3 = "assets/img/onb3.svg";
 }
 
