59dc91366fcdeebd1755833e089bdd3fdf5bfd64
ALIMUHAMMADALIHABAT <habatali64@gmail.com>
2024-08-05 21:21:54 +0300

    add path imge and and class image

---
+++ lib/core/constant/image.dart
@@ -0,0 +1,8 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImgLogoipsum255 1
+  /// assets/img/logoipsum-255 1.svg
+  static const String assetsImgLogoipsum2551 = "assets/img/logoipsum-255 1.svg";
+}
+
